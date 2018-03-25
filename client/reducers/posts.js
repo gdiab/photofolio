7237eba72dcb2891742dcdcd567b7c3869bf15c9
George Diab <georgediab@goto10solutions.com>
2018-03-25 16:14:14 -0700

    using connect

---
+++ client/reducers/posts.js
@@ -0,0 +1,14 @@
+// a reducer takes in two things
+
+// 1. the action (info about what happend)
+
+// and
+
+// 2. copy of current state
+
+function posts(state = [], action) {
+	console.log(state, action);
+	return state;
+}
+
+export default posts
